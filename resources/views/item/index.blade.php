dc54a9b4cb760fa80445a60d7503c66acd3941ea
Gegham Ayvazyan <gagvirus92@gmail.com>
2018-12-06 21:22:06 +0400

    Using @each directive for displaying list of items

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -8,17 +8,7 @@
 
             <div class="card-body">
                 <div class="row">
-                    @if($models->count())
-                        @foreach($models as $model)
-                            @include('item._item', ['model' => $model])
-                        @endforeach
-                    @else
-                        <div class="col-sm-12">
-                            <h4 class="text-danger">
-                                No posts found
-                            </h4>
-                        </div>
-                    @endif
+                    @each('item._item', $models, 'model', 'item._empty')
                 </div>
             </div>
         </div>
