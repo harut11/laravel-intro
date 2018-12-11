e568af920eead5be3e461da4c817354eaff79736
Gegham Ayvazyan <gagvirus92@gmail.com>
2018-12-11 20:52:02 +0400

    Added accessors for full name, age and short name. Added detail relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,6 +10,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
+
+    public static function boot()
+    {
+        self::created(function($model) {
+            $model->details()->save(new UserDetails());
+        });
+        parent::boot();
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -31,5 +40,40 @@ class User extends Authenticatable
     public function items()
     {
         return $this->hasMany(Item::class, 'owner_id', 'id');
+    }
+
+    public function details()
+    {
+        return $this->hasOne(UserDetails::class, 'id', 'id');
+    }
+
+    public function getShortNameAttribute()
+    {
+        if (!empty($this->details->attributes['first_name']) && !empty($this->details->attributes['last_name'])) {
+            $f = $this->details->attributes['first_name'];
+            $l = $this->details->attributes['last_name'];
+            return substr($f, 0, 1) . '. ' . $l;
+        }
+        return null;
+    }
+
+    public function getFullNameAttribute()
+    {
+        if (!empty($this->details->attributes['first_name']) && !empty($this->details->attributes['last_name']) && !empty($this->details->attributes['middle_name'])) {
+            $f = $this->details->attributes['first_name'];
+            $l = $this->details->attributes['last_name'];
+            $m = $this->details->attributes['middle_name'];
+            return "$f $m $l";
+        }
+        return null;
+    }
+
+    public function getAgeAttribute()
+    {
+        if (!empty($this->details->attributes['date_of_birth'])) {
+            $dob = $this->details->attributes['date_of_birth'];
+            $carbon = Carbon::createFromFormat('Y-m-d', $dob);
+            return $carbon->diffInYears();
+        }
     }
 }
