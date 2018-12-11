f3f5f3d3bf4389904bcf7f633812f07ca42719ab
Gegham Ayvazyan <gagvirus92@gmail.com>
2018-12-11 22:13:22 +0400

    Using shorthand data-method helper for logout

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -51,15 +51,9 @@
                             <a href="{{ route('user.profile') }}" class="dropdown-item">
                                 Profile
                             </a>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
+                            <a class="dropdown-item" href="{{ route('logout') }}" data-method="post">
                                 {{ __('Logout') }}
                             </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
                         </div>
                     </li>
                 @endguest
