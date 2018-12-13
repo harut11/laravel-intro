41d482acaa20d957693954305560e5d14bc3df4d
Gegham Ayvazyan <gagvirus92@gmail.com>
2018-12-13 20:33:35 +0400

    Sluggable Configuration for item category

--- app/Models/ItemCategory.php
+++ app/Models/ItemCategory.php
@@ -2,12 +2,27 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class ItemCategory extends Model
 {
+    use Sluggable;
     public function getSlugAttribute(): string
     {
     	return str_slug($this->name);
     }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    abstract public function sluggable() {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }: array
 }
